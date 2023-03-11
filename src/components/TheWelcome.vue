9c70e81d115235be746d06a1f5e3fd73a3226268
theburningmonk <theburningmonk@gmail.com>
2023-03-11 23:34:00 +0100

    feat: init commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,13 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template>Documentation</template>
+
+    Vue’s
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
+    provides you with all information you need to get started.
+  </WelcomeItem>
+</template>
