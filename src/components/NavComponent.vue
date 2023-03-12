cd4c970426a422e94b83add693a7fdebfe4ea494
theburningmonk <theburningmonk@gmail.com>
2023-03-12 15:32:58 +0100

    feat: footer

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="bg-white border-b border-gray-200 mb-10">
-    <div class="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-full mx-auto px-8">
       <div class="flex h-10">
         <div class="my-px ml-6 flex space-x-8">
           <a href="/otp" class="text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
--- src/views/OtpView.vue
+++ src/views/OtpView.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import { Auth } from 'aws-amplify'
 import Nav from '../components/NavComponent.vue'
+import Footer from '../components/FooterComponent.vue'
 
 const email = ref('')
 const password = ref('')
@@ -77,79 +78,86 @@ async function signOut() {
 </script>
 
 <template>
-  <Nav/>
+  <div class="flex-row grow justify-between">
+    <Nav/>
 
-  <h1 class="font-bold text-4xl mb-12">Passwordless login with OTP with Cognito</h1>
+    <div>
+      <h1 class="font-bold text-4xl mb-12">Passwordless login with OTP with Cognito</h1>
 
-  <div class="w-1/2 flex-row mb-10">
-    <h2 class="font-semibold text-xl">Step 1. register a user (if you haven't already)</h2>
-    <p>Password requirements: 8 chars (lower case, number, upper case + symbol)</p>
+      <div class="w-1/2 flex-row mb-10">
+        <h2 class="font-semibold text-xl">Step 1. register a user (if you haven't already)</h2>
+        <p>Password requirements: 8 chars (lower case, number, upper case + symbol)</p>
 
-    <input 
-      v-model="email" 
-      type="text"
-      class="mt-2 w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-      placeholder="Email address">
-    <input 
-      v-model="password" 
-      type="password"
-      class="mt-1 w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-      placeholder="Password">
-    <input 
-      v-if="signUpStep === 'CONFIRM'"
-      v-model="verificationCode" 
-      type="text"
-      class="mt-1 w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-      placeholder="Verification code (check email)">
+        <input 
+          v-model="email" 
+          type="text"
+          class="mt-2 w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+          placeholder="Email address">
+        <input 
+          v-model="password" 
+          type="password"
+          class="mt-1 w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+          placeholder="Password">
+        <input 
+          v-if="signUpStep === 'CONFIRM'"
+          v-model="verificationCode" 
+          type="text"
+          class="mt-1 w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+          placeholder="Verification code (check email)">
 
-    <button 
-      v-if="signUpStep === 'SIGN_UP'"
-      @click="signUp" 
-      class="mt-2 py-2 px-4 border border-transparent font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-      Create user
-    </button>
-    <button 
-      v-if="signUpStep === 'CONFIRM'"
-      @click="confirmUser"
-      class="mt-2 py-2 px-4 border border-transparent font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-      Confirm user
-    </button>
+        <button 
+          v-if="signUpStep === 'SIGN_UP'"
+          @click="signUp" 
+          class="mt-2 py-2 px-4 border border-transparent font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          Create user
+        </button>
+        <button 
+          v-if="signUpStep === 'CONFIRM'"
+          @click="confirmUser"
+          class="mt-2 py-2 px-4 border border-transparent font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          Confirm user
+        </button>
+      </div>
+
+      <div class="w-1/2 flex-row mb-10">
+        <h2 class="font-semibold text-xl">Step 2. sign in with OTP code</h2>
+
+        <input 
+          v-model="email" 
+          type="text"
+          class="mt-2 w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+          placeholder="Email address">
+        <input 
+          v-if="signInStep === 'CUSTOM_CHALLENGE'"
+          v-model="secretCode" 
+          type="text"
+          class="mt-1 w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+          placeholder="OTP code (check email)">
+
+        <button v-if="signInStep === 'SIGN_IN'"
+          @click="signIn"
+          class="mt-2 py-2 px-4 border border-transparent font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          Passwordless sign in
+        </button>
+        <button v-if="signInStep === 'CUSTOM_CHALLENGE'"
+          @click="answerCustomChallenge" 
+          class="mt-2 py-2 px-4 border border-transparent font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          Submit
+        </button>
+      </div>
+
+      <div v-if="isSignedIn" class="w-1/2 flex-row mb-10">
+        <h2 class="font-semibold text-xl">Step 3. you're signed in! Sign out and try again.</h2>
+
+        <button v-if="isSignedIn"
+          @click="signOut" 
+          class="mt-2 py-2 px-4 border border-transparent font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          Sign out
+        </button>
+      </div>
+    </div>
+
+    <Footer/>
   </div>
-
-  <div class="w-1/2 flex-row mb-10">
-    <h2 class="font-semibold text-xl">Step 2. sign in with OTP code</h2>
-
-    <input 
-      v-model="email" 
-      type="text"
-      class="mt-2 w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-      placeholder="Email address">
-    <input 
-      v-if="signInStep === 'CUSTOM_CHALLENGE'"
-      v-model="secretCode" 
-      type="text"
-      class="mt-1 w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-      placeholder="OTP code (check email)">
-
-    <button v-if="signInStep === 'SIGN_IN'"
-      @click="signIn"
-      class="mt-2 py-2 px-4 border border-transparent font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-      Passwordless sign in
-    </button>
-    <button v-if="signInStep === 'CUSTOM_CHALLENGE'"
-      @click="answerCustomChallenge" 
-      class="mt-2 py-2 px-4 border border-transparent font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-      Submit
-    </button>
-  </div>
-
-  <div v-if="isSignedIn" class="w-1/2 flex-row mb-10">
-    <h2 class="font-semibold text-xl">Step 3. you're signed in! Sign out and try again.</h2>
-
-    <button v-if="isSignedIn"
-      @click="signOut" 
-      class="mt-2 py-2 px-4 border border-transparent font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-      Sign out
-    </button>
-  </div>
+  
 </template>
