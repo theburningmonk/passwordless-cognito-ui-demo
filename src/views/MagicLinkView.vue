29d60741a156fdcab1d33da39447ca9beb8d505c
theburningmonk <theburningmonk@gmail.com>
2023-03-12 15:36:26 +0100

    feat: footer to magic link page

--- src/views/MagicLinkView.vue
+++ src/views/MagicLinkView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import Nav from '../components/NavComponent.vue'
+import Footer from '../components/FooterComponent.vue'
 </script>
 
 <template>
   <Nav/>
 
   <h1 class="font-bold text-4xl mb-12">Coming soon</h1>
+
+  <Footer/>
 </template>
